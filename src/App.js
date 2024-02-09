fd36a8b07cfc37a46ac27bead7c93b4c482fb2f5
Deshan001 <venomassassin01@gmail.com>
2024-02-09 11:38:03 +0530

    Upadte frontend side

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignUp />} />
         </Routes>
-        <Hero />
+        {/* <Hero /> */}
       </BrowserRouter>
     </div>
   );
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Item from '../Components/Item/Item'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
 
 const Shop = () => {
   return (
     <div>
-      
+      <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewCollections/>
     </div>
   )
 }
